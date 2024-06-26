bbf8a607b1a802377d0582d394124ea9663a421f
Clutcher <clutcher.sorry@gmail.com>
2024-06-26 18:07:38 +0300

    Rework filter, add create/edit/delete posts system

--- NewsPortal_SF_Proj/news/views.py
+++ NewsPortal_SF_Proj/news/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+
 from .models import Post
 from .filter import PostFilter
+from .forms import PostForm
 
 
 class PostList(ListView):
@@ -8,6 +11,7 @@ class PostList(ListView):
     ordering = '-time_in'
     template_name = 'posts.html'
     context_object_name = 'post'
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,7 +29,7 @@ class PostSearch(ListView):
     ordering = '-time_in'
     template_name = 'search.html'
     context_object_name = 'post'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self, **kwargs):
         queryset = super().get_queryset()
@@ -36,4 +40,51 @@ class PostSearch(ListView):
         context = super().get_context_data(**kwargs)
         context['filterset'] = self.filterset
         return context
+
+
+class NewsCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.genre = 'NE'
+        return super().form_valid(form)
+
+
+class NewsEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+
+class ArticlesCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'articles_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.genre = 'AR'
+        return super().form_valid(form)
+
+
+class ArticlesEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'articles_edit.html'
+
+
+class ArticlesDelete(DeleteView):
+    model = Post
+    template_name = 'articles_delete.html'
+    success_url = reverse_lazy('posts_list')
+
 
